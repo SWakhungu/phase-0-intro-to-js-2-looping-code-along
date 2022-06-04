07ea0ce6f7c91418666cab91877f739ced08020a
Steve Wakhungu <stwakhungu@gmail.com>
2022-06-04 15:10:53 +0300

    commit tree e9a2314805eed1fca4e548a84c95baf80971c467

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"]
+
+function writeCards(names, event) {
+    let i = 0 
+    let messages = []
+    while(i < names.length) {   
+    let message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`
+    messages.push(message)
+    i++
+ }
+    return messages
+}
+
+function countDown(number) {
+    let i = number
+    while(i>=0){
+        console.log(i)
+        i--
+    }
+}
